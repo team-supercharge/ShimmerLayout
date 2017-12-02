04e099ae2f9767c9ac9e33321f8d1b27523c6103
veghtomi <tommi.vegh@gmail.com>
2017-12-02 02:55:26 +0100

    Update project

--- app/src/main/java/io/supercharge/shimmeringlayout/MainActivity.java
+++ app/src/main/java/io/supercharge/shimmeringlayout/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ShimmerLayout shimmerLayout = (ShimmerLayout) findViewById(R.id.shimmer_layout);
+        ShimmerLayout shimmerLayout = findViewById(R.id.shimmer_layout);
         shimmerLayout.startShimmerAnimation();
     }
 }
